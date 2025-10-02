5b38798409fd56889e7fd10642f64a768b4d300e
Patil-Aaditi <159870576+Patil-Aaditi@users.noreply.github.com>
2025-10-02 22:29:40 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -23,9 +23,9 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+MONGO_URL = os.environ['MONGO_URL']
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+DB_NAME = client[os.environ['DB_NAME']]
 
 print("Connecting to MongoDB...")
 try:
